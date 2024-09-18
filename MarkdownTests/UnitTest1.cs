4e8fa7689f343eaa624a75d5e776381a2390b8c7
D4rQ <lagodykk@mail.ru>
2024-09-18 19:53:31 +0300

    Added test project

---
+++ MarkdownTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MarkdownTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
